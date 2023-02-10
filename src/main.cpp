a4bf9cae22dc8cb792d57458b104d69a50387d39
Romain Hoarau <rohoarau@student.42lausanne.ch>
2023-02-10 13:12:28 +0100

    clean start on test-server-1

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include "../inc/webserv.hpp"
-// server in c++ from scratch using select()
+// server/client communication in c++ from scratch using select()
+
+void    error(std::string msg) {
+    perror(msg.c_str());
+    exit (EXIT_FAILURE);
+}
 
 /*
     socket(): create a socket - Get the file descriptor!
@@ -10,51 +15,8 @@
     shutdown to end read.
     close to releases data.
  */
-void    error(std::string msg) {
-    perror(msg.c_str());
-    exit (EXIT_FAILURE);
-}
-
 void    run_server(void) {
-    struct sockaddr_in  serv_addr;
-    struct timeval      tv;
-    fd_set              fd;
-    int                 socket_fd;
-    int                 accept_fd;
-    int                 ret;
-
-    if ((socket_fd = socket(AF_INET, SOCK_STREAM /*TCP Stream*/, 0)) < 0)
-        error("socket(): fatal.\n");
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(LOCALHOST);
-    serv_addr.sin_port = htons(PORT);
-
-    bind(socket_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-
-    if (listen(socket_fd, 1) < 0)
-        error("listen(): fatal.\n");
-
-    while (true) {
-        FD_ZERO(&fd);
-        FD_SET(socket_fd, &fd);
-        ret = select(socket_fd + 1, &fd, NULL, NULL, &tv);
-
-        if (ret == 0)
-            error("select(): timeout.");
-        else if (ret == 1)
-            error("select(): error.");
-
-        accept_fd = accept(socket_fd, (struct sockaddr*)NULL, NULL);
-
-        char pkt[1024];
-        if (recv(accept_fd, &pkt, 1024, 0) < 0)
-            error("recv(): error: failed to receive data");
-
-        std::cout << "Received data !" << std::endl << pkt << std::endl;
-
-        close(accept_fd);
-    }
+    return ;
 }
 
 void    run_client(void) {
@@ -64,10 +26,9 @@ void    run_client(void) {
 void    run_webserv(void) {
     pid_t   pid;
 
-    if ((pid = fork()) < 0) {
-        perror("fork(): fatal.");
-        exit (EXIT_FAILURE);
-    }
+    if ((pid = fork()) < 0)
+        error("fork(): fatal.");
+
     if (pid == 0)
         run_client();
     else {
