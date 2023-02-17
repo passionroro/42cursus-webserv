c1e560b5acc6b5d658b97fd01b16512bd5eadd67
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2023-02-17 18:08:05 +0100

    Add tester from intra

--- src/Config.hpp
+++ src/Config.hpp
@@ -1,4 +1,14 @@
 #ifndef CONFIG_HPP
 # define CONFIG_HPP
 
+# include "webserv.hpp"
+
+class	Config {
+
+public:
+
+private:
+
+};
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
-#include "../inc/webserv.hpp"
+#include "webserv.hpp"
+#include "Server.hpp"
 
-int main() {
-    return 1;
+int	main(int argc, char **argv)
+{
+	if (argc != 2) {
+		std::cerr << "Error: usage: " << argv[0] <<  "<config_file>" << std::endl;
+		return (1);
+	}
+	Server	server;
+	
 }
