f3bbab0cf759b8e8ca87a375895b86e924fecc3e
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2023-02-17 18:08:05 +0100

    Add tester from intra

--- inc/webserv.hpp
+++ inc/webserv.hpp
@@ -1,4 +1,17 @@
 #ifndef WEBSERV_HPP
 # define WEBSERV_HPP
 
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <arpa/inet.h>
+# include <stdlib.h>
+
+# include <string>
+# include <iostream>
+
+class	Server;
+class	Config;
+class	Request;
+class	Response;
+
 #endif
--- src/Server.hpp
+++ src/Server.hpp
@@ -1,4 +1,24 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
+# include "webserv.hpp"
+# include "Config.hpp"
+
+class	Server {
+
+public:
+
+	Server(std::string const & config_file);
+
+	void	run(void);
+
+
+private:
+
+	Config	_config;
+
+	Server(void);
+
+};
+
 #endif
