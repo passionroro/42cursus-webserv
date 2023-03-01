e243558f0e10c360e25cba3e0f9ba9e436f120e7
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2023-03-01 17:46:18 +0100

    Clean send, add Response class

--- src/Response.hpp
+++ src/Response.hpp
@@ -1,4 +1,19 @@
 #ifndef RESPONSE_HPP
 # define RESPONSE_HPP
 
+# include "webserv.hpp"
+
+class	Response {
+
+public:
+
+	Response(void);
+	virtual	~Response(void);
+	
+	std::string	createString(void);
+
+private:
+
+};
+
 #endif
