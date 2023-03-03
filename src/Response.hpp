fcc02a41595a100de482b837e2bb9ec708b190f9
amuhleth42 <96829299+amuhleth42@users.noreply.github.com>
2023-03-03 15:17:48 +0100

    Revert "Clean send, add Response class"
    This reverts commit e243558f0e10c360e25cba3e0f9ba9e436f120e7.

--- src/Response.hpp
+++ src/Response.hpp
@@ -1,19 +1,4 @@
 #ifndef RESPONSE_HPP
 # define RESPONSE_HPP
 
-# include "webserv.hpp"
-
-class	Response {
-
-public:
-
-	Response(void);
-	virtual	~Response(void);
-	
-	std::string	createString(void);
-
-private:
-
-};
-
 #endif
